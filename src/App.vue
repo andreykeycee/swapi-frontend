1cfcfd4d146ca4434377e6df999161271fc735d3
andrey <andrey@ob-vious.com>
2019-01-09 06:12:52 -0800

    done

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   .search
     h1.search-title Star Wars
     form.search-form(@keydown.enter.prevent="searchPeople()")
-      input.search-bar(type="text" v-model="search" list="autocomplete" @input="autocomplete()")
-      datalist#autocomplete
-        option(v-for="offer in autocompleteResponse.data") {{ offer.name }}
+      input.search-bar(type="text" v-model="search" @input="autocomplete()" @focus="showAutocomplete = true")
+      ul.autocomplete(v-show="showAutocomplete")
+        li(v-for="offer in autocompleteResponse.data" :key="offer.name" @click="search = offer.name, showAutocomplete = false") {{ offer.name }}
       button.search-button(type="button" @click="searchPeople()") Search
     .results(v-if="response")
       .result(v-for="result in response.data")
@@ -13,6 +13,10 @@
         | {{ result.species }}
         br
         | {{ result.homeworld }}
+        br
+        | Your match: {{ Math.floor(result.total) }}
+    .error-results(v-if="error") Sorry, your request have no results, choose one of these
+        .result(v-for="result in errorResponse.data" @click="search = result.name, searchPeople()") {{ result.name }}
 </template>
 
 <script>
@@ -24,15 +28,26 @@ export default {
     return {
       search: '',
       response: {},
-      autocompleteResponse: {}
+      errorResponse: {},
+      error: false,
+      autocompleteResponse: {},
+      showAutocomplete: false
     }
   },
   methods: {
     async searchPeople () {
       if (this.search) {
-        this.response = await axios.get(`http://localhost:3000/api/match/${this.search}`)
-        console.log(this.response.data)
+        const { data } = await axios.get(`http://localhost:3000/api/match/${this.search}`)
+        if (data.errors) {
+          this.response = {}
+          this.error = true
+          this.errorResponse = await axios.get(`http://localhost:3000/api/autocomplete/${this.search}`)
+        } else {
+          this.error = false
+          this.response = { data }
+        }
       }
+      this.showAutocomplete = false
     },
     async autocomplete () {
       if (this.search) {
@@ -45,14 +60,41 @@ export default {
 </script>
 
 <style lang="sass">
+
+
 .search
   display: flex
   flex-direction: column
   align-items: center
 
+.search-form
+  position: relative
+
+.search-bar
+  height: 15px
+
+.autocomplete
+  position: absolute
+  top: 0
+  left: -35px
+  list-style: none
+  margin-top: 22px
+  background: white
+  li:hover
+    cursor: pointer
+    background: lightgray
+
 .results
   padding: 20px
+
+.result
+  padding: 5px
+  border: 1px solid #f0f0f0
+
+.error-results
   .result
-    padding: 5px
-    border: 1px solid #f0f0f0
+    cursor: pointer
+    &:hover
+      background: lightgray
+
 </style>
